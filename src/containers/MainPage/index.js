538b2fab24bbfc15bb03934d5437a4dab870107b
antonbuyuk <anton.buyuk@abiatec.com>
2018-01-24 16:33:25 +0300

    Add MainPage

--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -1,18 +1,79 @@
 import React from 'react'
 
-import {  Layout } from 'antd'
+import { Layout } from 'antd'
 // const { Content } = Layout;
+import { Icon, Row, Col, TopPanel, TopPanelItem, TableNoHead, TableNoHeadItem, Reviews, TreatmentTable } from 'appdoc-component'
+import Hoc from '../../hoc'
 
+
+import './styles.css'
+import {dataArr, scheduleArr, treatmentArr} from './mock-data'
 
 class MainPage extends React.Component{
 
     render(){
 
-
         return (
 
             <Layout.Content>
-                Main Page
+                <Hoc>
+					<Row className='section top-panel'>
+						<div className='topPanel'>
+							<Col span={6} className='flexCol'>
+								<TopPanelItem
+									className='first-col'
+									PanelTitle="10 сентября 2017"
+									PanelText="Вторник  13:25"
+									svg
+									icon='calendar'
+								>
+								</TopPanelItem>
+							</Col>
+
+							<Col span={6} className='flexCol'>
+								<TopPanelItem
+								PanelTitle="Приемы сегодня"
+								PanelText="12"
+								icon='info'
+								>
+								</TopPanelItem>
+							</Col>
+
+							<Col span={6} className='flexCol'>
+								<TopPanelItem
+									PanelTitle="Актуальные обращения"
+									PanelText="2"
+									svg
+									icon='clock'
+								>
+								</TopPanelItem>
+							</Col>
+
+							<Col span={6} className='flexCol'>
+								<TopPanelItem
+									PanelTitle="Мои пациенты"
+									PanelText="37"
+									icon='user'
+								>
+								</TopPanelItem>
+							</Col>
+						</div>
+					</Row>
+
+					<Row className="section">
+						<Col span={14} className='flexCol'>
+							<TableNoHead data={scheduleArr}/>
+						</Col>
+						<Col span={10} className='flexCol'>
+							<Reviews data={dataArr}/>
+						</Col>
+					</Row>
+					<Row className="section">
+						<Col span={24} className='flexCol'>
+							<TreatmentTable data={treatmentArr}/>
+						</Col>
+					</Row>
+                </Hoc>
             </Layout.Content>
         )
     }
