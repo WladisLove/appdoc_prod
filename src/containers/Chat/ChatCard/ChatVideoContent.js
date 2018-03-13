fd9f59572e0c3e4b8968302d15ba4786ef6e74a1
WladisLove <klim12121997@yandex.by>
2018-03-13 14:20:33 +0300

    all workint and in 1 class

--- src/containers/Chat/ChatCard/ChatVideoContent.js
+++ src/containers/Chat/ChatCard/ChatVideoContent.js
@@ -10,7 +10,12 @@ import { Button, Radio, ChatFiles, ChatSend, ChatMessage, ChatVideoPanel } from 
 import './style.css'
 
 
-/*var ws = new WebSocket('wss://' + 'localhost:8443' + '/one2one');
+//import { Icon, Row, Col, HistoryReceptions } from 'appdoc-component'
+import Hoc from '../../../hoc'
+
+
+
+
 var videoInput;
 var videoOutput;
 var webRtcPeer;
@@ -21,238 +26,255 @@ const REGISTERING = 1;
 const REGISTERED = 2;
 var registerState = null
 
-function setRegisterState(nextState) {
-	switch (nextState) {
-	case REGISTERED:
-		setCallState(NO_CALL);
-		break;
 
-	default:
-		return;
-	}
-	registerState = nextState;
-}
 
 const NO_CALL = 0;
 const PROCESSING_CALL = 1;
 const IN_CALL = 2;
 var callState = null
 
-function setCallState(nextState) {
-	callState = nextState;
-}
-
-window.ws = ws;
-
-window.onload = function() {
-	setRegisterState(NOT_REGISTERED);	
-}
-
-window.onbeforeunload = function() {
-	ws.close();
-}
-
-ws.onmessage = function(message) {
-	var parsedMessage = JSON.parse(message.data);
-	console.info('Received message: ' + message.data);
-
-	switch (parsedMessage.id) {
-	case 'registerResponse':
-		resgisterResponse(parsedMessage);
-		break;
-	case 'callResponse':
-		callResponse(parsedMessage);
-		break;
-	case 'incomingCall':
-		incomingCall(parsedMessage);
-		break;
-	case 'startCommunication':
-		startCommunication(parsedMessage);
-		break;
-	case 'stopCommunication':
-		console.info("Communication ended by remote peer");
-		stop(true);
-		break;
-	case 'iceCandidate':
-		webRtcPeer.addIceCandidate(parsedMessage.candidate)
-		break;
-	default:
-		console.error('Unrecognized message', parsedMessage);
-	}
-}
-
-function resgisterResponse(message) {
-	if (message.response == 'accepted') {
-		setRegisterState(REGISTERED);
-	} else {
-		setRegisterState(NOT_REGISTERED);
-		var errorMessage = message.message ? message.message
-				: 'Unknown reason for register rejection.';
-		console.log(errorMessage);
-		alert('Error registering user. See console for further information.');
-	}
-}
-
-function callResponse(message) {
-	if (message.response != 'accepted') {
-		console.info('Call not accepted by peer. Closing call');
-		var errorMessage = message.message ? message.message
-				: 'Unknown reason for call rejection.';
-		console.log(errorMessage);
-		stop(true);
-	} else {
-		setCallState(IN_CALL);
-		webRtcPeer.processAnswer(message.sdpAnswer);
-	}
-}
-
-function startCommunication(message) {
-	setCallState(IN_CALL);
-	webRtcPeer.processAnswer(message.sdpAnswer);
-}
-
-function incomingCall(message) {
-	// If bussy just reject without disturbing user
-	if (callState != NO_CALL) {
-		var response = {
-			id : 'incomingCallResponse',
-			from : message.from,
-			callResponse : 'reject',
-			message : 'bussy'
-
-		};
-		return sendMessage(response);
-	}
-
-	setCallState(PROCESSING_CALL);
-	if (window.confirm('User ' + message.from
-	+ ' is calling you. Do you accept the call?')) {
-
-		var options = {
-			localVideo : videoInput,
-			remoteVideo : videoOutput,
-			onicecandidate : onIceCandidate
-		}
-		
-
-		webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options,
-				function(error) {
-					if (error) {
-						console.error(error);
-						setCallState(NO_CALL);
-					}
-
-					this.generateOffer(function(error, offerSdp) {
-						if (error) {
-							console.error(error);
-							setCallState(NO_CALL);
-						}
-						var response = {
-							id : 'incomingCallResponse',
-							from : message.from,
-							callResponse : 'accept',
-							sdpOffer : offerSdp
-						};
-						console.log('[ATTENTION]');
-						sendMessage(response);
-					});
-				});
-
-	} else {
-		var response = {
-			id : 'incomingCallResponse',
-			from : message.from,
-			callResponse : 'reject',
-			message : 'user declined'
-		};
-		sendMessage(response);
-		stop(true);
-	}
-}
-
-function register() {
-	var name = document.getElementById('name').value;
-	console.log(document.getElementById('name'))
-	if (name == '') {
-		window.alert("You must insert your user name");
-		return;
-	}
-
-	setRegisterState(REGISTERING);
-
-	var message = {
-		id : 'register',
-		name : name
-	};
-	sendMessage(message);
-	document.getElementById('peer').focus();
-}
 
 
 
-function stop(message) {
-	setCallState(NO_CALL);
-	if (webRtcPeer) {
-		webRtcPeer.dispose();
-		webRtcPeer = null;
 
-		if (!message) {
-			var message = {
-				id : 'stop'
-			}
-			sendMessage(message);
-		}
-	}
-}
 
-function sendMessage(message) {
-	ws.send(JSON.stringify(message));
-}
 
-function onIceCandidate(candidate) {
-	var message = {
-		id : 'onIceCandidate',
-		candidate : candidate
-	}
-	sendMessage(message);
-}*/
 
- 
+
+
+
+
+
+
+
+
+
+
 
 
 class ChatVideoContent extends React.Component {
-    
+	constructor(props){
+		super(props);
 
-    /*call = () => {
+		this.ws = new WebSocket('wss://' + 'localhost:8443' + '/one2one');
+		this.ws.onmessage = (message) => {
+			var parsedMessage = JSON.parse(message.data);
+			console.info('Received message: ' + message.data);
+		
+			switch (parsedMessage.id) {
+			case 'registerResponse':
+				this.resgisterResponse(parsedMessage);
+				break;
+			case 'callResponse':
+				this.callResponse(parsedMessage);
+				break;
+			case 'incomingCall':
+				this.incomingCall(parsedMessage);
+				break;
+			case 'startCommunication':
+				this.startCommunication(parsedMessage);
+				break;
+			case 'stopCommunication':
+				console.info("Communication ended by remote peer");
+				this.stop(true);
+				break;
+			case 'iceCandidate':
+				webRtcPeer.addIceCandidate(parsedMessage.candidate)
+				break;
+			default:
+				console.error('Unrecognized message', parsedMessage);
+			}
+		}
+		window.ws = this.ws;
+	}
+
+	componentDidMount(){
+		this.setRegisterState(NOT_REGISTERED);
+	}
+
+	componentWillUnmount(){
+		this.ws.close();
+	}
+
+	setRegisterState = (nextState) => {
+		switch (nextState) {
+		case REGISTERED:
+			this.setCallState(NO_CALL);
+			break;
+	
+		default:
+			return;
+		}
+		registerState = nextState;
+	}
+
+	setCallState = (nextState) => {
+		callState = nextState;
+	}
+
+	resgisterResponse = (message) => {
+		if (message.response == 'accepted') {
+			this.setRegisterState(REGISTERED);
+		} else {
+			this.setRegisterState(NOT_REGISTERED);
+			var errorMessage = message.message ? message.message
+					: 'Unknown reason for register rejection.';
+			console.log(errorMessage);
+			window.alert('Error registering user. See console for further information.');
+		}
+	}
+
+	callResponse = (message) => {
+		if (message.response != 'accepted') {
+			console.info('Call not accepted by peer. Closing call');
+			var errorMessage = message.message ? message.message
+					: 'Unknown reason for call rejection.';
+			console.log(errorMessage);
+			this.stop(true);
+		} else {
+			this.setCallState(IN_CALL);
+			webRtcPeer.processAnswer(message.sdpAnswer);
+		}
+	}
+
+	startCommunication = (message) => {
+		this.setCallState(IN_CALL);
+		webRtcPeer.processAnswer(message.sdpAnswer);
+	}
+
+	incomingCall = (message) => {
+		// If bussy just reject without disturbing user
+		if (callState != NO_CALL) {
+			var response = {
+				id : 'incomingCallResponse',
+				from : message.from,
+				callResponse : 'reject',
+				message : 'bussy'
+	
+			};
+			return this.sendMessage(response);
+		}
+	
+		this.setCallState(PROCESSING_CALL);
+		if (window.confirm('User ' + message.from
+		+ ' is calling you. Do you accept the call?')) {
+	
+			var options = {
+				localVideo : videoInput,
+				remoteVideo : videoOutput,
+				onicecandidate : this.onIceCandidate
+			}
+			
+			let that = this;
+	
+			webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options,
+					function(error) {
+						if (error) {
+							console.error(error);
+							this.setCallState(NO_CALL);
+						}
+	
+						this.generateOffer(function(error, offerSdp) {
+							if (error) {
+								console.error(error);
+								this.setCallState(NO_CALL);
+							}
+							var response = {
+								id : 'incomingCallResponse',
+								from : message.from,
+								callResponse : 'accept',
+								sdpOffer : offerSdp
+							};
+							that.sendMessage(response);
+						});
+					});
+	
+		} else {
+			var response = {
+				id : 'incomingCallResponse',
+				from : message.from,
+				callResponse : 'reject',
+				message : 'user declined'
+			};
+			this.sendMessage(response);
+			this.stop(true);
+		}
+	}
+
+	register = () => {
+		var name = document.getElementById('name').value;
+		console.log(document.getElementById('name'))
+		if (name == '') {
+			window.alert("You must insert your user name");
+			return;
+		}
+	
+		this.setRegisterState(REGISTERING);
+	
+		var message = {
+			id : 'register',
+			name : name
+		};
+		this.sendMessage(message);
+		document.getElementById('peer').focus();
+	}
+
+	stop = (message) => {
+		this.setCallState(NO_CALL);
+		if (webRtcPeer) {
+			webRtcPeer.dispose();
+			webRtcPeer = null;
+	
+			if (!message) {
+				var message = {
+					id : 'stop'
+				}
+				this.sendMessage(message);
+			}
+		}
+	}
+
+	sendMessage = (message) => {
+		this.ws.send(JSON.stringify(message));
+	}
+
+	onIceCandidate = (candidate) => {
+		var message = {
+			id : 'onIceCandidate',
+			candidate : candidate
+		}
+		this.sendMessage(message);
+	}
+	
+	call = () => {
         if (document.getElementById('peer').value == '') {
             window.alert("You must specify the peer name");
             return;
         }
     
-        setCallState(PROCESSING_CALL);
+        this.setCallState(PROCESSING_CALL);
     
     
         var options = {
             localVideo : videoInput,
             remoteVideo : videoOutput,
-            onicecandidate : onIceCandidate
-        }
+            onicecandidate : this.onIceCandidate
+		}
+		let that = this;
     
         webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options, function(
                 error) {
             if (error) {
                 console.error(error);
-                setCallState(NO_CALL);
+                this.setCallState(NO_CALL);
             }
-            console.log('------------------------------')
-            console.log(this)
-            console.log('------------------------------')
+
     
     
             this.generateOffer(function(error, offerSdp) {
                 if (error) {
                     console.error(error);
-                    setCallState(NO_CALL);
+                    this.setCallState(NO_CALL);
                 }
                 var message = {
                     id : 'call',
@@ -260,35 +282,50 @@ class ChatVideoContent extends React.Component {
                     to : document.getElementById('peer').value,
                     sdpOffer : offerSdp
                 };
-                sendMessage(message);
+                that.sendMessage(message);
             });
         });
     
-    }*/
+    }
+
+    
     
     render() {
         const {isActive, videoCalling, onVideoCallBegin, onVideoCallStop} = this.props;
         const dialogsClass = cn('chat-card-dialogs', {'chat-card-dialogs-active': isActive});
 
-       /* const content = videoCalling
-            ?<div>   
-                <div>
+        return (
+
+            <div className={dialogsClass}>
+				<div>   
+                <div className="col-md-5">
+                    <label className="control-label">Name</label>
+                    <div className="row">
+                        <div className="col-md-6">
                         <input id="name" name="name" className="form-control" type="text"/>
-                        
-                        <a id="register" href="#" className="btn btn-primary" onClick={register}>
+                        </div>
+                        <div className="col-md-6 text-right">
+                        <a id="register" href="#" className="btn btn-primary" onClick={this.register}>
                             <span className="glyphicon glyphicon-plus"></span> Register</a>
+                        </div>
+                    </div>
                     <br/>
                     <br/>
                     <label className="control-label">Peer</label>
-                    
+                    <div className="row">
+                        <div className="col-md-6">
                         <input id="peer" name="peer" className="form-control" type="text"/>
-                        
+                        </div>
+                        <div className="col-md-6 text-right">
                         <a id="call" href="#" className="btn btn-success" onClick={this.call}>
                             <span className="glyphicon glyphicon-play"></span> Call</a>
-                        <a id="terminate" href="#" className="btn btn-danger" onClick = {stop}>
+                        <a id="terminate" href="#" className="btn btn-danger" onClick = {this.stop}>
                             <span className="glyphicon glyphicon-stop"></span> Stop</a>
-                        
+                        </div>
                     </div>
+                    <br/>
+                    </div>
+                    
             <div className='chat-card-message__area'>
                     
                     <video className='chat-card-video__box' 
@@ -300,17 +337,10 @@ class ChatVideoContent extends React.Component {
                             ref={video => {videoInput = video;}}
                             poster="https://i.ytimg.com/vi/gLa1sVtgGyI/maxresdefault.jpg"></video>
                     <div className='chat-card-video__panel'>
-                        <ChatVideoPanel  duration='00:00:34' onStop={stop}/>
+                        <ChatVideoPanel  duration='00:00:34' onStop={this.stop}/>
                     </div>
                 </div>
                 </div>
-            : <Button btnText='Call'
-                    onClick={this.call}
-            />*/
-
-        return (
-
-            <div className={dialogsClass}>
                             </div>
 
         )
