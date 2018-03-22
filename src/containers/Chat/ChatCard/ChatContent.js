7e383d747eb1917f85d7521b660e21ba44532d59
antonbuyuk <anton.buyuk@abiatec.com>
2018-03-22 15:43:38 +0300

    fix chat

--- src/containers/Chat/ChatCard/ChatContent.js
+++ src/containers/Chat/ChatCard/ChatContent.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import cn from 'classnames'
 
 
-import { Button, Radio,ChatFiles, ChatSend, ChatMessage } from 'appdoc-component'
+import { Button, Radio, ChatFiles, ChatSend, ChatMessage, ChatComments } from 'appdoc-component'
 
 import './style.css'
 
@@ -16,14 +16,21 @@ class ChatContent extends React.Component {
 
             <div className={dialogsClass}>
                 <div className='chat-card-message__area'>
-                    {/*<div className='chat-card-message__comments'>
+                    <div className='chat-card-message__comments'>
                      <ChatComments
                      comments="Жалоба пациента или комментарий к приему. Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах."
                      />  
-                     </div>*/}
+                     </div>
 
                     <div className='chat-card-message__box'>
                         <div className='chat-card-message__overlay'>
+                            <div className='btn-start'>
+                                <Button
+                                    btnText='Начать приём'
+                                    size='small'
+                                    type='yellow'
+                                />
+                            </div>
                             <ChatMessage
                                 img="https://www.proza.ru/pics/2017/06/03/1990.jpg"
                                 message="Здарова!"
