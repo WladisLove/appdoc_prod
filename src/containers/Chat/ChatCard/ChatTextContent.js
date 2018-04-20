3ff50decfbb7752d460d54b2aa9c177129327fd2
WladisLove <klim12121997@yandex.by>
2018-04-20 14:12:22 +0300

    general ws listeners in indexJS. Use them in ChatText

--- src/containers/Chat/ChatCard/ChatTextContent.js
+++ src/containers/Chat/ChatCard/ChatTextContent.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import kurentoUtils from 'kurento-utils'
 
 import PropTypes from 'prop-types'
-import cn from 'classnames'
 
 import { Button, Radio, ChatFiles, ChatSend, ChatMessage, ChatVideoPanel } from 'appdoc-component'
 import ChatContent from './ChatContent'
@@ -14,160 +12,33 @@ import './style.css'
 import Hoc from '../../../hoc'
 
 
-
-var registerName = null;
-const NOT_REGISTERED = 0;
-const REGISTERING = 1;
-const REGISTERED = 2;
-var registerState = null
-
-
-
-const NO_CALL = 0;
-const PROCESSING_CALL = 1;
-const IN_CALL = 2;
-var callState = null
-
-
-
 class ChatTextContent extends React.Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			from: 0,
-			to: 0,
-			isCalling: false,
 			isActive: this.props.isActive,
-			duration: 0,
-			timer: {
-				s: 0,
-				m: 0,
-				h: 0,
-			},
 			chatStory: [],
 		}
 
 		//this.ws = new WebSocket(props.wsURL);
 		this.ws = props.ws;
-		this.ws.onmessage = (message) => {
-			var parsedMessage = JSON.parse(message.data);
-		
-			switch (parsedMessage.id) {
-			case 'registerResponse':
-				this.resgisterResponse(parsedMessage);
-				break;
-			case 'chat':
-				this.setState({chatStory: [...this.state.chatStory, parsedMessage]})
-				break;
-			default:
-				console.error('Unrecognized message', parsedMessage);
-			}
-		}
-	}
-
-	timer = () => {
-		this.state.timer.s >= 59 
-			? (
-				this.state.timer.m >= 59 
-					? (
-						this.setState(prev => ({timer: {
-							h: prev.timer.h +1,
-							s: 0,
-							m: 0,
-						}}))
-					)
-					: this.setState(prev => ({timer: {
-						...prev.timer,
-						m: prev.timer.m +1,
-						s: 0,
-					}}))
-			)
-			: 
-			this.setState(prev => ({timer: {
-				...prev.timer,
-				s: prev.timer.s +1,
-			}}));
-	}
-
-	componentWillMount(){
-		this.register();
-
-		const answer = window.prompt('Enter ID to call');
-        if (answer == '') {
-            window.alert("You must specify the peer name");
-            return;
-		}
-		this.setState({to: answer});
-
-		
-	}
-
-
-	componentWillUnmount(){
-		//this.ws.close();
-	}
-
-	setRegisterState = (nextState) => {
-		switch (nextState) {
-		case REGISTERED:
-			this.setCallState(NO_CALL);
-			break;
-	
-		default:
-			return;
-		}
-		registerState = nextState;
-	}
-
-	setCallState = (nextState) => {
-		callState = nextState;
-	}
-
-	resgisterResponse = (message) => {
-		if (message.response == 'accepted') {
-			this.setRegisterState(REGISTERED);
-		} else {
-			this.setRegisterState(NOT_REGISTERED);
-			var errorMessage = message.message ? message.message
-					: 'Unknown reason for register rejection.';
-			console.log(errorMessage);
-			window.alert('Error registering user. See console for further information.');
-		}
-	}
-
-	register = () => {
-		let a = window.prompt();
-		let name = a ? a : ''+this.props.callerID;
-		console.log('[my ID is]', name)
-		
-		this.setState({from: name});	
-		this.setRegisterState(REGISTERING);
-	
-		this.ws.onopen = () => this.sendMessage({
-			id : 'register',
-			name : name
-		});
-	}
-
-	sendMessage = (message) => {
-		this.ws.send(JSON.stringify(message));
 	}
     
     
     render() {
-		console.log('from:', this.state.from, 'to', this.state.to)
+		console.log('from:', this.props.from, 'to', this.props.to)
         const {isActive} = this.props;
 
         return (
-            <ChatContent onSend={mes => this.sendMessage({
+            <ChatContent onSend={mes => this.props.sendMessage({
 						 id: 'chat',
-						 from: this.state.from,
-						 to: this.state.to,
+						 from: this.props.from,
+						 to: this.props.to,
 						 ...mes,
                      })}
                      isActive = {isActive}
-                     data={this.state.chatStory}
+                     data={this.props.chatStory}
                      
 		    />
         )
@@ -176,10 +47,12 @@ class ChatTextContent extends React.Component {
 
 ChatTextContent.propTypes = {
 	wsURL: PropTypes.string.isRequired,
+	sendMessage: PropTypes.func,
 };
 
 ChatTextContent.defaultProps = {
 	wsURL: '',
+	sendMessage: () => {},
 };
 
 export default ChatTextContent
--- src/containers/Chat/ChatCard/index.js
+++ src/containers/Chat/ChatCard/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import kurentoUtils from 'kurento-utils'
+
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 
@@ -11,14 +13,74 @@ import ChatVideoContent from './ChatVideoContent'
 
 import './style.css'
 
+var videoInput;
+var videoOutput;
+var webRtcPeer;
+
+var registerName = null;
+const NOT_REGISTERED = 0;
+const REGISTERING = 1;
+const REGISTERED = 2;
+var registerState = null
+
+
+
+const NO_CALL = 0;
+const PROCESSING_CALL = 1;
+const IN_CALL = 2;
+var callState = null
+
+
 class ChatCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isActive: this.props.isActive,
             mode: this.props.mode,
+
+            from: 0,
+			to: 0,
+			isCalling: false,
+			duration: 0,
+			timer: {
+				s: 0,
+				m: 0,
+				h: 0,
+			},
+			chatStory: [],
         }
-        this.ws = new WebSocket(props.wsURL)
+        this.ws = new WebSocket(props.wsURL);
+        this.ws.onmessage = (message) => {
+			var parsedMessage = JSON.parse(message.data);
+			console.info('Received message: ' + message.data);
+		
+			switch (parsedMessage.id) {
+			case 'registerResponse':
+				this.resgisterResponse(parsedMessage);
+				break;
+			case 'callResponse':
+				this.callResponse(parsedMessage);
+				break;
+			case 'incomingCall':
+				this.incomingCall(parsedMessage);
+				break;
+			case 'startCommunication':
+				this.startCommunication(parsedMessage);
+				break;
+			case 'stopCommunication':
+				console.info("Communication ended by remote peer");
+				this.stop(true);
+				break;
+			case 'chat':
+				this.setState({chatStory: [...this.state.chatStory, parsedMessage]})
+				break;
+			case 'iceCandidate':
+				webRtcPeer.addIceCandidate(parsedMessage.candidate)
+				break;
+			default:
+				console.error('Unrecognized message', parsedMessage);
+			}
+		}
     }
 
     filesRender = (dataArr) => {
@@ -31,6 +93,252 @@ class ChatCard extends React.Component {
         return filesArr;
     };
 
+    timer = () => {
+		this.state.timer.s >= 59 
+			? (
+				this.state.timer.m >= 59 
+					? (
+						this.setState(prev => ({timer: {
+							h: prev.timer.h +1,
+							s: 0,
+							m: 0,
+						}}))
+					)
+					: this.setState(prev => ({timer: {
+						...prev.timer,
+						m: prev.timer.m +1,
+						s: 0,
+					}}))
+			)
+			: 
+			this.setState(prev => ({timer: {
+				...prev.timer,
+				s: prev.timer.s +1,
+			}}));
+	}
+
+	componentWillMount(){
+        this.register();
+
+		const answer = window.prompt('Enter ID to call');
+        if (answer == '') {
+            window.alert("You must specify the peer name");
+            return;
+        }
+        this.setState({to: answer});
+	}
+
+	componentWillUnmount(){
+		clearInterval(this.timerInterval);
+		this.ws.close();
+	}
+
+	setRegisterState = (nextState) => {
+		switch (nextState) {
+		case REGISTERED:
+			this.setCallState(NO_CALL);
+			break;
+	
+		default:
+			return;
+		}
+		registerState = nextState;
+	}
+
+	setCallState = (nextState) => {
+		nextState === IN_CALL ? this.timerInterval = setInterval(this.timer, 1000) : null;
+		callState = nextState;
+	}
+
+	resgisterResponse = (message) => {
+		if (message.response == 'accepted') {
+			this.setRegisterState(REGISTERED);
+		} else {
+			this.setRegisterState(NOT_REGISTERED);
+			var errorMessage = message.message ? message.message
+					: 'Unknown reason for register rejection.';
+			console.log(errorMessage);
+			window.alert('Error registering user. See console for further information.');
+		}
+	}
+
+	callResponse = (message) => {
+		if (message.response != 'accepted') {
+			console.info('Call not accepted by peer. Closing call');
+			var errorMessage = message.message ? message.message
+					: 'Unknown reason for call rejection.';
+			console.log(errorMessage);
+			this.stop(true);
+		} else {
+			this.setCallState(IN_CALL);
+			webRtcPeer.processAnswer(message.sdpAnswer);
+		}
+	}
+
+	startCommunication = (message) => {
+		this.setCallState(IN_CALL);
+		webRtcPeer.processAnswer(message.sdpAnswer);
+	}
+
+	incomingCall = (message) => {
+		// If bussy just reject without disturbing user
+		if (callState != NO_CALL) {
+			var response = {
+				id : 'incomingCallResponse',
+				from : message.from,
+				callResponse : 'reject',
+				message : 'bussy'
+	
+			};
+			return this.sendMessage(response);
+		}
+	
+		this.setCallState(PROCESSING_CALL);
+		if (window.confirm('User ' + message.from
+		+ ' is calling you. Do you accept the call?')) {
+			this.setState({isCalling: true, to: message.from})
+	
+			var options = {
+				localVideo : videoInput,
+				remoteVideo : videoOutput,
+				onicecandidate : this.onIceCandidate
+			}
+			
+			let that = this;
+	
+			webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options,
+					function(error) {
+						if (error) {
+							console.error(error);
+							this.setCallState(NO_CALL);
+						}
+	
+						this.generateOffer(function(error, offerSdp) {
+							if (error) {
+								console.error(error);
+								this.setCallState(NO_CALL);
+							}
+							var response = {
+								id : 'incomingCallResponse',
+								from : message.from,
+								callResponse : 'accept',
+								sdpOffer : offerSdp
+							};
+							that.sendMessage(response);
+						});
+					});
+	
+		} else {
+			var response = {
+				id : 'incomingCallResponse',
+				from : message.from,
+				callResponse : 'reject',
+				message : 'user declined'
+			};
+			this.sendMessage(response);
+			this.stop(true);
+		}
+	}
+
+	register = () => {
+		let a = window.prompt();
+		let name = a ? a : ''+this.props.callerID;
+		console.log('[my ID is]', name)
+		
+		this.setState({from: name});	
+		this.setRegisterState(REGISTERING);
+	
+		this.ws.onopen = () => this.sendMessage({
+			id : 'register',
+			name : name
+		});
+	}
+
+	stop = (message) => {
+		this.setState({isCalling: false});
+		clearInterval(this.timerInterval);
+		this.setState({timer: {
+			s: 0,
+			m: 0,
+			h: 0,
+		}})
+		this.setCallState(NO_CALL);
+		if (webRtcPeer) {
+			webRtcPeer.dispose();
+			webRtcPeer = null;
+	
+			if (!message) {
+				var message = {
+					id : 'stop'
+				}
+				this.sendMessage(message);
+			}
+		}
+	}
+
+	sendMessage = (message) => {
+		this.ws.send(JSON.stringify(message));
+	}
+
+	onIceCandidate = (candidate) => {
+		var message = {
+			id : 'onIceCandidate',
+			candidate : candidate
+		}
+		this.sendMessage(message);
+	}
+	
+	call = () => {
+		this.setState({isCalling: true})
+		this.setCallState(PROCESSING_CALL);
+    
+        let options = {
+            localVideo : videoInput,
+            remoteVideo : videoOutput,
+            onicecandidate : this.onIceCandidate
+		}
+		let that = this;
+    
+        webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options, function(
+                error) {
+            if (error) {
+                console.error(error);
+                this.setCallState(NO_CALL);
+            }
+
+	
+			const {from, to} = that.state;
+    
+            this.generateOffer(function(error, offerSdp) {
+                if (error) {
+                    console.error(error);
+                    this.setCallState(NO_CALL);
+                }
+                var message = {
+                    id : 'call',
+                    from,
+                    to,
+                    sdpOffer : offerSdp
+                };
+                that.sendMessage(message);
+            });
+        });
+    
+    }
+
+	onCall = () => {
+		const answer = window.prompt('Enter ID to call');
+        if (answer == '') {
+            window.alert("You must specify the peer name");
+            return;
+		}
+		this.setState({to: answer});
+		
+		this.call();
+	}
+	onStop = () => {
+		this.stop();
+	}
 
     render() {
         const {patientName, online} = this.props;
@@ -49,6 +357,10 @@ class ChatCard extends React.Component {
             case 'chat':
                 content = <ChatTextContent isActive={this.state.isActive} 
                                             ws={this.ws} 
+                                            from={this.state.from}
+                                            to={this.state.to}
+                                            chatStory={this.state.chatStory}
+                                            sendMessage = {this.sendMessage}
                                             {...this.props}/>;
                 break;
             case 'voice':
