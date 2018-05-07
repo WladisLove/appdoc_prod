5e0aaec909c213e902c123fa5819741f61c51fdb
WladisLove <klim12121997@yandex.by>
2018-05-07 15:43:48 +0300

    package update

--- src/containers/Chat/mock-data.js
+++ src/containers/Chat/mock-data.js
@@ -1,5 +1,33 @@
 export const dialogArr = [
     {
+        id: 126012,
+        fio: "тестовый пациент",
+        avatar: "https://178.172.235.105/media/img/zaglushka.jpg",
+        start: "1525692821",
+        end: 1523598900,
+        id_doc: "2697",
+        id_user: "2663",
+        del: "1",
+        comment: "Подозрения на бронхит",
+        delComment: "",
+        diagnosis: null,
+        type: "video"
+    },
+    {
+        id: 126013,
+        fio: "тестовый пациент",
+        avatar: "https://178.172.235.105/media/img/zaglushka.jpg",
+        start: "1525692821",
+        end: 1523598900,
+        id_doc: "2697",
+        id_user: "2663",
+        del: "1",
+        comment: "Подозрения на бронхит",
+        delComment: "",
+        diagnosis: null,
+        type: "video"
+    }
+    /*{
         name: "Иванова А. К.",
         img: "https://www.proza.ru/pics/2017/06/03/1990.jpg",
         time: '15:00',
@@ -21,5 +49,5 @@ export const dialogArr = [
         consultation: 'Консультация по результатам анализа asdasdasds',
         status: "default",
         iconType: "chat1",
-    }
+    }*/
 ];
