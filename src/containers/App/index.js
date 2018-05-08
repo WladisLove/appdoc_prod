ed13f0d858b4559e5eea4f9a2abd3a7bd7c79f35
WladisLove <klim12121997@yandex.by>
2018-05-08 11:52:24 +0300

    default wide work space

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -19,7 +19,7 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            collapsed: false,
+            collapsed: true,
         };
     }
 
