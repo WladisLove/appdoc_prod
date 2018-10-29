9af41d8539e3e79c76970c01230a4d9c8aebd614
WladisLove <klim12121997@yandex.by>
2018-10-29 13:03:51 +0300

    fix no audio in audio call

--- src/containers/App/chatWs.js
+++ src/containers/App/chatWs.js
@@ -371,6 +371,7 @@ export const call = () => {
                     audio:true,
                     video:false
                 },
+                remoteVideo : videoOutput,
                 onicecandidate : onIceCandidate
             };
 
