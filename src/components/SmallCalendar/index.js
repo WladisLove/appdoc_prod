22aa869a43307838bbdcc5d2bd14f27b42d87c16
Andrei <ansolaris9@gmail.com>
2019-01-22 16:36:45 +0300

    Set locale for SmallCalendar component

--- src/components/SmallCalendar/index.js
+++ src/components/SmallCalendar/index.js
@@ -14,6 +14,7 @@ class SmallCalendar extends React.Component {
                           value={date}
                           nextLabel={<Icon type="arrow_right" size={10} svg/>}
                           prevLabel={<Icon type="arrow_left" size={10} svg/>}
+                          locale="en-US"
                           tileClassName={({date, view}) => {
                               if (highlightedDates && isUser) {
                                   return view === 'month' && (highlightedDates.indexOf(moment(date).format("YYYY MM DD")) + 1) ? 'highlighted-day' : null
