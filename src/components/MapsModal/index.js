1c4e4308e51c1ff9e8fc9b80ce7b1b66276279ae
Roman <iromjkeecorporation@gmail.com>
2019-03-14 11:55:59 +0300

    add location of pacient to doctor during  reception

--- src/components/MapsModal/index.js
+++ src/components/MapsModal/index.js
@@ -4,11 +4,14 @@ import PropTypes from 'prop-types'
 import Icon from '../Icon'
 import Modal from '../Modal'
 import './styles.css'
+import {Map, Placemark, YMaps} from "react-yandex-maps";
+
+const mapState = { center: [53.90, 27.55], zoom: 12};
 
 class MapsModal extends React.Component{
 
     render(){
-        const {visible, title, warning, width} = this.props;
+        const {visible, title, warning, width, height} = this.props;
         
         return (
             <Modal visible={visible}
@@ -19,6 +22,15 @@ class MapsModal extends React.Component{
                       closable = {!warning}
                       onCancel={this.props.onCancel}
             >
+                <YMaps>
+                    <Map state={mapState} width={'100%'} height={height}>
+                        <Placemark
+                            geometry={{
+                                coordinates: [53.90, 27.55]
+                            }}
+                        />
+                    </Map>
+                </YMaps>
                 {warning && <Icon type="caution" svg size={24}/>}
                 {this.props.children}
             </Modal>
