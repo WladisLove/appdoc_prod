3ad753c7111ae442187ef6c37c7bfa17f8002fe8
latiertiw <latierti@yandex.ru>
2019-08-09 01:05:28 +0300

    fix header markup

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -253,7 +253,7 @@ class App extends React.Component {
                                         emergencyAvailable={this.props.emergencyAvailable}
                                         docSpecialities={this.props.docSpecialities}
                                         userBalance={this.props.userBalance}
-
+                                        isShort={!this.state.collapsed}
                                     />
                                 </div>
                                 <div className="main-content">
